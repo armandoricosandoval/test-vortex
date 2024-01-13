82c1e57fd1ff3cb75a21997b12c13faa97dd0406
arman <armando.rico@tempedge.com>
2024-01-13 04:56:00 -0500

    se ajustan rutas y configuraciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
 import store from "./redux/store";
 import AppRouters from "./routers/AppRouters";
 
 function App() {
   return (
-    <HashRouter>
-      <Provider store={store}>
-        <AppRouters />
-      </Provider>
-    </HashRouter>
+    <Provider store={store}>
+      <AppRouters />
+    </Provider>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  routes: {
+    "/": "./src/App.jsx",
+    "/about": "./src/About.tsx",
+    "/login": "./src/pages/Login.jsx", // Add other routes as needed
+    "/sing-up": "./src/pages/Singup.jsx",
+    "/profile-students": "./src/pages/ProfileStudents.jsx",
+    "/writers": "./src/pages/Writers.jsx",
+  },
+});
