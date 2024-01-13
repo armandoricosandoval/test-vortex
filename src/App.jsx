1e0a4d93db3c639ef3a07b8efca752f1178a312b
arman <armando.rico@tempedge.com>
2024-01-13 04:37:05 -0500

    se crea branch para publicar app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import AppRouters from './routers/AppRouters';
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import store from "./redux/store";
+import AppRouters from "./routers/AppRouters";
 
 function App() {
-
   return (
-    <Provider store={store}>
-      <AppRouters />
-    </Provider>
-  )
+    <HashRouter>
+      <Provider store={store}>
+        <AppRouters />
+      </Provider>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
